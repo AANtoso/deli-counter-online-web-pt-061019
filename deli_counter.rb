c07cd4a8acdc25a1cdd194721915b70518791dc0
Adjoa Ntoso <adjoaan@gmail.com>
2019-06-27 23:06:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.empty?
+  puts "The line is currently empty."
+  else
+    line = "The line is currently:"
+    katz_deli.each.with_index(1) do |name, num|
+      line << " #{num}. #{name}"
+    end
+  puts line
+  end
+end
+
+def take_a_number(katz_deli, person)
+  katz_deli = "Ada"
+  return take_a_number
+end
